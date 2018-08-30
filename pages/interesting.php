7323d2c26664d2e2ff75c72a517933007f2f6eae
Sh1l0 <andrey.shilkin2010@yandex.ru>
2018-08-30 09:35:07 +0700

    Статьи в интересном

--- pages/interesting.php
+++ pages/interesting.php
@@ -19,5 +19,20 @@
 		<input type="text" name="search2" placeholder="Испарятор 3000"style="box-shadow:  3px 5px 10px grey inset; border: 0px;">
 		<input type="submit" name="search2"  value="Найти" id="searchButton2">
 	</form></div>
+	<div id="coolPubs2">
+		<div>
+			<span class="content3"><span>dzvzxcv</span><br><img src="https://cdn3.volusion.com/myrsg.dceqt/v/vspfiles/photos/SLVASK-2.jpg?1530271916" align="left">asdasda dfsjd asjfis jdsfsiodfjasiodf f aidhf asid fhais dfhiasdhf oias dhfiuas dhfioaus dhf oasdfha isdf eidja oif jasdo3ifjds okfjasdk lfjkasdljfaskdlfjasdf iasud hf asjdhfasd ljf hlasd flsdfll dajsld sal dfgds fhghjkasdgfjhsd gjhasdgsd sdasdasd</span>
+		</div>
+		<div>
+			<span class="content3"><span>dzvzxcv</span><br><img src="https://cdn3.volusion.com/myrsg.dceqt/v/vspfiles/photos/SLVASK-2.jpg?1530271916" align="left">asdasda dfsjd asjfis jdsfsiodfjasiodf f aidhf asid fhais dfhiasdhf oias dhfiuas dhfioaus dhf oasdfha isdf eidja oif jasdo3ifjds okfjasdk lfjkasdljfaskdlfjasdf iasud hf asjdhfasd ljf hlasd flsdfll dajsld sal dfgds fhghjkasdgfjhsd gjhasdgsd sdasdasd</span>
+		</div>
+		<div>
+			<span class="content3"><span>dzvzxcv</span><br><img src="https://cdn3.volusion.com/myrsg.dceqt/v/vspfiles/photos/SLVASK-2.jpg?1530271916" align="left">asdasda dfsjd asjfis jdsfsiodfjasiodf f aidhf asid fhais dfhiasdhf oias dhfiuas dhfioaus dhf oasdfha isdf eidja oif jasdo3ifjds okfjasdk lfjkasdljfaskdlfjasdf iasud hf asjdhfasd ljf hlasd flsdfll dajsld sal dfgds fhghjkasdgfjhsd gjhasdgsd sdasdasd</span>
+		</div>
+		<div>
+			<span class="content3"><span>dzvzxcv</span><br><img src="https://cdn3.volusion.com/myrsg.dceqt/v/vspfiles/photos/SLVASK-2.jpg?1530271916" align="left">asdasda dfsjd asjfis jdsfsiodfjasiodf f aidhf asid fhais dfhiasdhf oias dhfiuas dhfioaus dhf oasdfha isdf eidja oif jasdo3ifjds okfjasdk lfjkasdljfaskdlfjasdf iasud hf asjdhfasd ljf hlasd flsdfll dajsld sal dfgds fhghjkasdgfjhsd gjhasdgsd sdasdasd</span>
+		</div>
+		
+	</div>
 	</div>
 </body>
